2dd46be69adb1229f5b30057ae82fec35935720b
Luis Cuya <luis.cuya@intelica.com>
2025-05-28 16:38:00 -0500

    feat: add propiedades de posible listas de eventos de dominios

--- src/Usuarios/Usuarios.Domain/Abstractions/Entity.cs
+++ src/Usuarios/Usuarios.Domain/Abstractions/Entity.cs
@@ -3,5 +3,22 @@ namespace Usuarios.Domain.Abstractions;
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> events = new();
     public Guid Id { get; set; }
+    public void RaiseEvent(IDomainEvent domainEvent)
+    {
+        if (domainEvent == null)
+        {
+            throw new ArgumentNullException(nameof(domainEvent));
+        }
+        events.Add(domainEvent);
+    }
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return events.ToList();
+    }
+    public void ClearDomainEvents()
+    {
+        events.Clear();
+    }
 }
