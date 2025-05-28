64c06100f276e01260340d5e0e30ccb11711c6a4
Luis Cuya <luis.cuya@intelica.com>
2025-05-28 14:49:22 -0500

    feat: se crea Solucion Usuarios, libreria de clases Usuarios.Domain y dentro de esto se instala MediatR, para aplicar publicacion y subscripcion

--- src/Usuarios/Usuarios.Domain/Abstractions/IDomainEvent.cs
+++ src/Usuarios/Usuarios.Domain/Abstractions/IDomainEvent.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 
 namespace Usuarios.Domain.Abstractions
 {
-    public interface IDomainEvent
+    public interface IDomainEvent : INotification
     {
-        
+         
     }
 }
