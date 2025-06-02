9148112cb7608eafc16f87612011a6cb6b44f6cf
Luis Cuya <luis.cuya@intelica.com>
2025-06-02 11:23:01 -0500

    feat: se agrega la entidad rol con sus propiedades

--- src/Usuarios/Usuarios.Domain/Roles/Rol.cs
+++ src/Usuarios/Usuarios.Domain/Roles/Rol.cs
@@ -1,5 +1,28 @@
-﻿namespace Usuarios.Domain.Roles;
+﻿using Usuarios.Domain.Abstractions;
 
-public class Rol
+namespace Usuarios.Domain.Roles;
+
+public class Rol : Entity
 {
+    private Rol(
+        Guid id,
+        string nombreRol,
+        string descripcion
+        ) : base(id)
+    {
+        NombreRol = nombreRol;
+        Descripcion = descripcion;
+    }
+
+    public string NombreRol { get; private set; }
+    public string Descripcion { get; private set; }
+
+    public static Result<Rol> Create(
+        Guid id,
+        string nombreRol,
+        string descripcion
+        )
+    {
+        return Result.Success(new Rol(id, nombreRol, descripcion));
+    }
 }
