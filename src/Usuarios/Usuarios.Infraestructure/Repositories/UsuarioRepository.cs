07d67795855434c360bfa6a37dbbb902136e503c
Luis Cuya <luis.cuya@intelica.com>
2025-06-04 21:32:37 -0500

    feat: se agrega usuarioRepository y se hereda el Repository de clase generico para realizar el CRUD

---
+++ src/Usuarios/Usuarios.Infraestructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using Usuarios.Domain.Usuarios;
+
+namespace Usuarios.Infraestructure.Repositories;
+
+internal sealed class UsuarioRepository : Repository<Usuario>, IUsuarioRepository
+{
+    public UsuarioRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
