660235cadf89e023d4bc76eaa2e07eb806e52c81
Luis Cuya <luis.cuya@intelica.com>
2025-06-04 21:21:26 -0500

    feat: add dbcontext y repositorio generico, en el dbcontext se toma el savechange de EF, una vez que se persiste la data en BD se traen los eventos de dominios que esten guardados de N entidades y se retorna

---
+++ src/Usuarios/Usuarios.Infraestructure/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Usuarios.Domain.Abstractions;
+
+namespace Usuarios.Infraestructure;
+
+public class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    private readonly IPublisher _publisher;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+    IPublisher publisher)
+        : base(options)
+    {
+        _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            await PublishDomainEventsAsync();
+            return result;
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEntities = ChangeTracker
+        .Entries<Entity>()
+        .Select(entry => entry.Entity)
+        .SelectMany(entity =>
+        {
+            var domainEvents = entity.GetDomainEvents();
+            entity.ClearDomainEvents();
+            return domainEvents;
+        }).ToList();
+
+        foreach (var domainEvent in domainEntities)
+        {
+            await _publisher.Publish(domainEvent, CancellationToken.None);
+        }
+    }
+}
