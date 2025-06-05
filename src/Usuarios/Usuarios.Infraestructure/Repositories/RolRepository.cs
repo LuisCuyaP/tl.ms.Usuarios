0d6f0b90170fd7ea2e8c64ff517d9bf1ad47daa7
Luis Cuya <luis.cuya@intelica.com>
2025-06-04 21:35:48 -0500

    feat: se agrega RolRepository y aca  solo deseo heredar el metodo GetByNameAsync por ello implemento el metodo de la interfaz IRolRepository(click), ya no heredo(click) de la clase Repository Generico

---
+++ src/Usuarios/Usuarios.Infraestructure/Repositories/RolRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Usuarios.Domain.Roles;
+
+namespace Usuarios.Infraestructure.Repositories;
+
+internal sealed class RolRepository : Repository<Rol>, IRolRepository
+{
+    public RolRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Rol?> GetByNameAsync(string nombreRol, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Rol>()
+            .FirstOrDefaultAsync(rol => rol.NombreRol == nombreRol, cancellationToken);
+    }
+}
