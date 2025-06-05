d1b807b6093ec3d94157204aebcadae857140b49
Luis Cuya <luis.cuya@intelica.com>
2025-06-04 20:43:58 -0500

    feat: add CrearUsuarioDomainEventHandler se va a ejecutar siempre que se genere un nuevo evento de UserCreateDomainEvent que esta dentro de usuario entity (capa dominio), se crear un usuario se publica el evento y luego se suscribe para poder enviar un email de bienvenida al usuario, en inyeccion de dependencia agregar mediatR para la escucha entre capas

---
+++ src/Usuarios/Usuarios.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Usuarios.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(
+        string to,
+        string subject,
+        string body);
+}
---
+++ src/Usuarios/Usuarios.Application/Usuarios/CrearUsuario/CrearUsuarioDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Usuarios.Application.Abstractions.Email;
+using Usuarios.Domain.Events;
+using Usuarios.Domain.Usuarios;
+
+namespace Usuarios.Application.Usuarios.CrearUsuario;
+
+public class CrearUsuarioDomainEventHandler : INotificationHandler<UserCreateDomainEvent>
+{
+    private readonly IEmailService _emailService;
+    private readonly IUsuarioRepository _usuarioRepository;
+
+    public CrearUsuarioDomainEventHandler(IEmailService emailService, IUsuarioRepository usuarioRepository)
+    {
+        _emailService = emailService;
+        _usuarioRepository = usuarioRepository;
+    }
+
+    public async Task Handle(UserCreateDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var usuario = await _usuarioRepository.GetByIdAsync(notification.IdUsuario, cancellationToken);
+        if (usuario is null)
+        {
+            return;
+        }
+
+        await _emailService.SendEmailAsync(
+            usuario.CorreoElectronico.Value,
+            "Bienvenido al sistema",
+            $"Hola {usuario.NombresPersona}, tu cuenta ha sido creada exitosamente."
+        );
+    }
+}
