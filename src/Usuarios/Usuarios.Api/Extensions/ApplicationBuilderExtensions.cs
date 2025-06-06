b6abb77f05b093f48043a78a77afdcdfc7257650
Luis Cuya <luis.cuya@intelica.com>
2025-06-06 17:30:45 -0500

    feat: clase de extension para la migracion de EF, genera instancia de ApplicationDbContext y empieza migracion

---
+++ src/Usuarios/Usuarios.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Usuarios.Api.Middleware;
+using Usuarios.Infraestructure;
+
+namespace Usuarios.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static async void ApplyMigrations(
+        this IApplicationBuilder app
+    )
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "Error en la migracion de dominio a la base de datos");
+                throw;
+            }
+        }
+    }
+
+    public static void UseCustomExceptionHandler(
+        this IApplicationBuilder app
+    )
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
