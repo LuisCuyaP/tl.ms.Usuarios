4296a15858fbd01153de1eb89767646e678f70e6
Luis Cuya <luis.cuya@intelica.com>
2025-06-07 17:03:18 -0500

    feat: add fluent validation a los commands insert

---
+++ src/Usuarios/Usuarios.Application/Usuarios/CrearUsuario/CrearUsuarioCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Usuarios.Application.Usuarios.CrearUsuario;
+
+public class CrearUsuarioCommandValidator : AbstractValidator<CrearUsuarioCommand>
+{
+    public CrearUsuarioCommandValidator()
+    {
+        RuleFor(u => u.CorreoElectronico).NotEmpty()
+        .WithMessage("El corrreo electronico no puede ser vacio");
+
+        RuleFor(u => u.ApellidoPaterno).NotEmpty();
+
+        RuleFor(u => u.NombresPersona).NotEmpty();
+
+        RuleFor(u => u.FechaNacimiento).NotEmpty();
+
+        RuleFor(u => u.FechaNacimiento).LessThan(DateTime.Now)
+        .WithMessage("La fecha de nacimiento no es valida");
+    }
+}
